074e2ff5bdd1de9325c9edaa247a53deeec73e47
Mike <villegas.jose.miguel.1005@gmail.com>
2026-02-22 19:36:43 -0600

    Entrega Tarea 4(3

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,20 +1,17 @@
 module Helper exposing (..)
 
-import Html
-import Html.Attributes
+import Char
+import Html exposing (Html, a, div, h1, h2, h3, h4, h5, h6, text)
+import Html.Attributes exposing (href)
 
 
 
--- Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
--- main : Html.Html msg
--- main =
---     Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
--- headers : String -> Html msg
--- headers titulo =
---     Html.h1 [] [ text titulo ]
--- hyperlink : String -> String -> Html msg
--- hyperlink url etiqueta =
---     Html.a [ href url ] [ text etiqueta ]
+--Puedes usar una definición como la siguiente para probar y visualizar tus resultados, solo debes definir "headers" y "hyperlink"
+
+
+main : Html.Html msg
+main =
+    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
 
 
 type GradeStatus
@@ -66,3 +63,35 @@ airplaneScheduleAction status =
 airportAction : List AirplaneStatus -> List String
 airportAction statuses =
     List.map airplaneScheduleAction statuses
+
+
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+headers : String -> Html msg
+headers param =
+    div []
+        [ h1 [] [ text param ]
+        , h2 [] [ text param ]
+        , h3 [] [ text param ]
+        , h4 [] [ text param ]
+        , h5 [] [ text param ]
+        , h6 [] [ text param ]
+        ]
+
+
+hyperlink : String -> String -> Html msg
+hyperlink url linkText =
+    a [ href url ] [ text linkText ]
